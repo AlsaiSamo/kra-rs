f70e0912a37eb2c3dd57b0cdc0a6a71b26e74d10
Alsai Samo <alsai_imikoy@proton.me>
2024-02-03 03:43:09 +0700

    Small flake cleanup

--- flake.nix
+++ flake.nix
@@ -15,56 +15,47 @@
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, crane, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
-        rustNightly = pkgs.rust-bin.selectLatestNightlyWith (t: t.default);
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustNightly;
-        my-crate = craneLib.buildPackage {
-          src = craneLib.cleanCargoSource (craneLib.path ./.);
-          buildInputs = [
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+      rustNightly = pkgs.rust-bin.selectLatestNightlyWith (t: t.default);
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustNightly;
+      my-crate = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+        buildInputs = []
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
             pkgs.libiconv
           ];
+      };
+    in {
+      checks = {
+        inherit my-crate;
+      };
 
-          # Additional environment variables can be set directly
-          # MY_CUSTOM_VAR = "some value";
-        };
-      in
-      {
-        checks = {
-          inherit my-crate;
-        };
+      packages.default = my-crate;
 
-        packages.default = my-crate;
+      devShells.default = pkgs.mkShell {
+        inputsFrom = builtins.attrValues self.checks.${system};
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = my-crate;
-        };
+        RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues self.checks.${system};
-
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-          RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
-
-          # Extra inputs can be added here
-          nativeBuildInputs = with pkgs; [
-            #cargo
-            #rustc
-            #bacon
-            rustNightly
-            clippy
-            rustfmt
-            rust-analyzer
-            sccache
-            # stdenv
-          ];
-        };
-      });
+        nativeBuildInputs = with pkgs; [
+          rustNightly
+          clippy
+          rustfmt
+          rust-analyzer
+          sccache
+        ];
+      };
+    });
 }
